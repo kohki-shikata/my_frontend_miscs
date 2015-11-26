def5449e02dd7f305f01491c442d6f1332b53314
kohki <shikata@n-di.co.jp>
2015-11-26 10:55:32 +0900

    initial commit

---
+++ js/to_top.js
@@ -0,0 +1,8 @@
+$('#toTop').click(function(){
+  var speed = 400;
+  var href= $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top;
+  $("html, body").animate({scrollTop:position}, speed, "swing");
+  return false;
+});
